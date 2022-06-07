5f228771a10c7a51f8b55ba626c1ae7557528d39
Ode98 <mrotto100@gmail.com>
2022-06-07 13:12:42 +0300

    Working version

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
     bot.send_message(message.chat.id, "/niilo näyttää kaikki mahdolliset clipit\n/ääniclipin_nimi antaa ääniclipin :3")
-    handle_delete(message)
 
 
 @bot.message_handler(commands=['gibe'])
@@ -27,27 +26,18 @@ def OldMemeGen(message):
 @bot.message_handler(commands=['niilo', 'niilo22', 'Niilo', 'Niilo22'])
 def handle_niilo_help(message):
     bot.send_message(message.chat.id, (', '.join(lines)))
-    handle_delete(message)
 
 
 @bot.message_handler(commands=list(lines.keys()))
 def handle_niilo_review(message):
-    voice = open(f'{NIILO_VOICE_PATH}{lines[message.text[1:]]}' , 'rb')
-    bot.send_voice(message.chat.id, voice)
+    sound = open(f'{NIILO_VOICE_PATH}\{lines[message.text[1:]]}' , 'rb')
+    bot.send_audio(message.chat.id, sound, '', '', f'{message.from_user.first_name}', f'{lines[message.text[1:]]}')
     handle_delete(message)
 
 
 @bot.message_handler(func=lambda message : message.text[0] == '/')
 def handle_delete(message):
     bot.delete_message(message.chat.id, message.id)
-
-
-def send_msg(message_info, message):
-    bot.send_message(message_info.chat.id, message)
-
-
-def niilo22_ohje():
-    return(list(lines.keys()))
 
 
 bot.infinity_polling()
